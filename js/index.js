d392f3333678e597e8d2517f83bbb3b540777635
sagnik2010 <sagnikkundu25@gmail.com>
2020-12-21 17:19:16 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var map
+
+
+function initMap() {
+    let losAngeles = { lat: 34.063380, lng: -118.358080 }
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: losAngeles,
+        zoom: 8
+    })
+    var marker = new google.maps.Marker({
+        position: { lat: 34.063380, lng: -118.358080 },
+        map: map
+    })
+}
